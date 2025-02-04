60465efdf76a7072939552ddf6f45a4ed31b8596
kyu <knh3136@naver.com>
2025-02-04 14:58:13 +0900

    스프링시큐리티 설정 및 특정주소허가

---
+++ src/main/java/com/secure/note/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.secure.note.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests
+                -> requests
+                .requestMatchers("/contact").permitAll()
+                .requestMatchers("/public/**").permitAll()
+                .anyRequest().authenticated());
+        //http.formLogin(withDefaults());
+        http.sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.httpBasic(withDefaults());
+
+        return http.build();
+    }
+}
