a86095ed6dceef97833e857ea630cbc66de847b0
kyu <knh3136@naver.com>
2025-02-04 14:57:42 +0900

    컨트롤러 테스트

--- src/main/java/com/secure/note/controllers/HelloController.java
+++ src/main/java/com/secure/note/controllers/HelloController.java
@@ -1,4 +1,28 @@
 package com.secure.note.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/contact")
+    public String sayContact(){
+        return "Contact";
+    }
+
+    @GetMapping("/public/abc")
+    public String publicAbc(){
+        return "인증필요없음";
+    }
+
+    @GetMapping("/hi")
+    public String sayHi(){
+        return "Hi";
+    }
 }
