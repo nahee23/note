d39e7e2bcaf67f56258659ac950184a8d17a76b0
kyu <knh3136@naver.com>
2025-02-06 10:45:05 +0900

    임시유저추가

--- src/main/java/com/secure/note/security/SecurityConfig.java
+++ src/main/java/com/secure/note/security/SecurityConfig.java
@@ -1,5 +1,11 @@
 package com.secure.note.security;
 
+import com.secure.note.models.AppRole;
+import com.secure.note.models.Role;
+import com.secure.note.models.User;
+import com.secure.note.repositories.RoleRepository;
+import com.secure.note.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +13,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.time.LocalDate;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -28,5 +36,47 @@ public class SecurityConfig {
         http.httpBasic(withDefaults());
 
         return http.build();
+    }
+
+    //CommandLineRunner 처음 실행시 한번 실행 유저롤과 테스트 유저1,2를 입력함.
+    @Bean
+    public CommandLineRunner initData(RoleRepository roleRepository,
+                                      UserRepository userRepository) {
+        return args -> { //실행되는 코드들
+            //권한 테이블에 유저 권한이 있으면 가져오고 없으면 입력함
+            Role userRole = roleRepository.findByRoleName(AppRole.ROLE_USER)
+                    .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_USER)));
+            //권한 테이블에 관리자 권한이 있으면 가져오고 없으면 입력함
+            Role adminRole = roleRepository.findByRoleName(AppRole.ROLE_ADMIN)
+                    .orElseGet(() -> roleRepository.save(new Role(AppRole.ROLE_ADMIN)));
+
+            if (!userRepository.existsByUserName("user1")) {
+                User user1 = new User("user1", "user1@example.com", "{noop}password1");
+                user1.setAccountNonLocked(false);
+                user1.setAccountNonExpired(true);
+                user1.setCredentialsNonExpired(true);
+                user1.setEnabled(true);
+                user1.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
+                user1.setAccountExpiryDate(LocalDate.now().plusYears(1));
+                user1.setTwoFactorEnabled(false);
+                user1.setSignUpMethod("email");
+                user1.setRole(userRole);
+                userRepository.save(user1);
+            }
+
+            if (!userRepository.existsByUserName("admin")) {
+                User admin = new User("admin", "admin@example.com", "{noop}adminPass");
+                admin.setAccountNonLocked(true);
+                admin.setAccountNonExpired(true);
+                admin.setCredentialsNonExpired(true);
+                admin.setEnabled(true);
+                admin.setCredentialsExpiryDate(LocalDate.now().plusYears(1));
+                admin.setAccountExpiryDate(LocalDate.now().plusYears(1));
+                admin.setTwoFactorEnabled(false);
+                admin.setSignUpMethod("email");
+                admin.setRole(adminRole);
+                userRepository.save(admin);
+            }
+        };
     }
 }
