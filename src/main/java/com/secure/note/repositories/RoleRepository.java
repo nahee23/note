e4535a5784e173f1a3a6136db1e4eeb54e532d44
kyu <knh3136@naver.com>
2025-02-06 10:07:52 +0900

    Integer로 수정

--- src/main/java/com/secure/note/repositories/RoleRepository.java
+++ src/main/java/com/secure/note/repositories/RoleRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface RoleRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, Integer> {
     //유저권한을 role 테이블에서 찾음
     Optional<Role> findByRoleName(AppRole appRole);
 }
