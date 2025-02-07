c7ad04aa25bc061349fa8a51c7c27335d4b6b02b
kyu <knh3136@naver.com>
2025-02-07 09:39:03 +0900

    url 제한

---
+++ src/main/java/com/secure/note/controllers/AdminController.java
@@ -0,0 +1,42 @@
+package com.secure.note.controllers;
+
+import com.secure.note.dtos.UserDTO;
+import com.secure.note.models.User;
+import com.secure.note.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+//@PreAuthorize("hasRole('ROLE_ADMIN')") //SecurityConfig 에서 설정
+public class AdminController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/getusers")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(),
+                HttpStatus.OK);
+    }
+
+    @PutMapping("/update-role")
+    public ResponseEntity<String> updateUserRole(@RequestParam Long userId,
+                                                 @RequestParam String roleName) {
+        userService.updateUserRole(userId, roleName);
+        return ResponseEntity.ok("User role updated");
+    }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<UserDTO> getUser(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.getUserById(id),
+                HttpStatus.OK);
+    }
+
+
+}
--- src/main/java/com/secure/note/security/SecurityConfig.java
+++ src/main/java/com/secure/note/security/SecurityConfig.java
@@ -8,10 +8,13 @@ import com.secure.note.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.time.LocalDate;
@@ -20,12 +23,14 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(requests
                 -> requests
+                .requestMatchers("/api/admin/**").hasRole("ADMIN")
                 .requestMatchers("/contact").permitAll()
                 .requestMatchers("/public/**").permitAll()
                 .anyRequest().authenticated());
@@ -78,5 +83,15 @@ public class SecurityConfig {
                 userRepository.save(admin);
             }
         };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CommandLineRunner initData(){
+
     }
 }
